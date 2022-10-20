451a082f3bb42f4697c1dccb58af141845ea362a
Hernandes <hernandes.santos@olist.com>
2022-10-20 17:44:27 -0300

    check version script accept args

--- check_version.sh
+++ check_version.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-export VERSION_LIB=$(poetry show -o | grep accounts-drf)
-if [ -z "$VERSION_LIB" ]
-then
-      echo "\$VERSION_LIB is ok"
-else
-      IFS=' ' read -r -a array <<< $VERSION_LIB
-      echo "accounts-drf outdated, please update accounts-drf ${array[1]} -> ${array[2]}"
-      exit 1
-fi
+for ARG in "$@"; do
+    export VERSION_LIB=$(poetry show -o | grep $ARG)
+    if [ -z "$VERSION_LIB" ]
+    then
+        echo "$VERSION_LIB is updated"
+    else
+        IFS=' ' read -r -a array <<< $VERSION_LIB
+        echo "$ARG outdated, please update $ARG ${array[1]} -> ${array[2]}"
+        exit 1
+    fi
+done    
